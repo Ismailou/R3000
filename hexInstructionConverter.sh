673a07a01a2a2aabc7644dfd232f04506d103f50
Ismail ZEMNI <ismailzemni@gmail.com>
2015-02-06 16:02:03 +0100

    enhance display: add source instruction

--- hexInstructionConverter.sh
+++ hexInstructionConverter.sh
@@ -5,15 +5,28 @@
 # ( used for debug purpose )
 ###############################################################################
 
-if [ "$#" -ne 1 ]; then
-    echo "Syntax error, please tape : ./hexInstructionConverter.sh mybinaryFile.bin"
+if [ "$#" -ne 2 ]; then
+    echo "Syntax error, please tape : ./hexInstructionConverter.sh myProgram.asm mybinary.bin"
     exit
 fi 
 
-PROG=$(cat $1)
+IFS=$'\n'
+arr=$(cat $1)
+count=0
+instruction[0]=010
+
+for i in ${arr[@]};
+do
+	instruction[$count]=$i;
+	count=$(( $count + 1 ))
+done
+
+PROG=$(cat $2)
+count=0
 
 for inst in $PROG;
 do
+		printf '%s\t|' "${instruction[$count]}"
 		printf '0x%x\n' "$((2#$inst))"
-    #echo $i
+    count=$(( $count + 1 ))
 done    
